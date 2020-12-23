236226c2ccaa5a23287050a6370eb6c2719c6fac
Atle H. Havsø <atle@havso.net>
2020-12-23 23:35:54 +0100

    Part 22.2 solved

--- day22.cs
+++ day22.cs
@@ -13,7 +13,7 @@ namespace AdventOfCode
     public class Day22
     {
         public class Player {
-            public List<int> Hand {get;} = new List<int>();
+            public List<int> Hand { get; set; } = new List<int>();
 
             public IEnumerable<int> Draw() {
                 var card = Hand.FirstOrDefault();
@@ -28,7 +28,80 @@ namespace AdventOfCode
 
             public override string ToString()
             {
-                return String.Join("\n", Hand);
+                return String.Join(",", Hand);
+            }
+        }
+
+        public class Game {
+            public Player POne { get; set; }
+            public Player PTwo { get; set; }
+            public List<List<int>> PreviousPOneHands { get; } = new List<List<int>>();
+            public List<List<int>> PreviousPTwoHands { get; } = new List<List<int>>();
+
+            private int round = 0;
+
+            public int Play(out int score) {
+                score = 0;
+                while(true) {
+                    round += 1;
+                    if(CheckSequenceEqual()) {
+                        Console.WriteLine($"Player 1 won by Rule 1 with score {POne.Score()}");
+                        score = POne.Score();
+                        return 1;
+                    }
+
+                    PreviousPOneHands.Add(POne.Hand.Select(x => x).ToList());
+                    PreviousPTwoHands.Add(PTwo.Hand.Select(x => x).ToList());
+
+                    var cardA = POne.Draw().FirstOrDefault();
+                    var cardB = PTwo.Draw().FirstOrDefault();
+
+                    if (POne.Hand.Count >= cardA && PTwo.Hand.Count >= cardB) {
+                        Console.WriteLine($"New recursive game");
+                        var game = new Game{
+                            POne = new Player {
+                                Hand = POne.Hand.Take(cardA).Select(x => x).ToList(),
+                            },
+                            PTwo = new Player {
+                                Hand = PTwo.Hand.Take(cardB).Select(x => x).ToList(),
+                            },
+                        };
+
+                        var result = game.Play(out var subScore);
+                        if (result == 1) {
+                            POne.Hand.Add(cardA);
+                            POne.Hand.Add(cardB);
+                        } else if (result == 2) {
+                            PTwo.Hand.Add(cardB);
+                            PTwo.Hand.Add(cardA);
+                        }
+                    } else {
+                        if (cardA > cardB) {
+                            POne.Hand.Add(cardA);
+                            POne.Hand.Add(cardB);
+                        } else {
+                            PTwo.Hand.Add(cardB);
+                            PTwo.Hand.Add(cardA);
+                        }
+                    }
+
+                    if (POne.Hand.Count == 0) {
+                        Console.WriteLine($"Game over, POne hand empty!");
+                        score = PTwo.Score();
+                        return 2;
+                    } else if (PTwo.Hand.Count == 0) {
+                        Console.WriteLine($"Game over, PTwo hand empty!");
+                        score = POne.Score();
+                        return 1;
+                    }
+                }
+            }
+
+            private bool CheckSequenceEqual() {
+                if(PreviousPOneHands.All(x => !x.SequenceEqual(POne.Hand)) && PreviousPTwoHands.All(x => !x.SequenceEqual(PTwo.Hand))) {
+                    return false;
+                }
+                return true;
             }
         }
 
@@ -88,6 +161,13 @@ namespace AdventOfCode
 
         private void Task2()
         {
+            var game = new Game {
+                POne = PlayerOne,
+                PTwo = PlayerTwo,
+            };
+
+            var result = game.Play(out var score);
+            Console.WriteLine($"Game over! Player {result} won with score {score}");
         }
 
         public void Run()
@@ -110,6 +190,7 @@ namespace AdventOfCode
             Console.WriteLine("\nLooking for answer for Task 1.");
             this.Task1();
             Console.WriteLine("\nLooking for answer for Task 2.");
+            this.ParseTodaysInput(s);
             this.Task2();
         }
     }
