5639b5a14acd6ed177ab10ed27936c148706333d
Atle H. Havsø <atle@havso.net>
2020-12-26 12:07:01 +0100

    Part 24.2 solved

--- day24.cs
+++ day24.cs
@@ -12,7 +12,49 @@ namespace AdventOfCode
 {
     public class Day24
     {
-        public SortedList<int, SortedList<int, char>> TileGrid = new SortedList<int, SortedList<int, char>>();
+        public class Tiles : IEnumerable<char>
+        {
+            private SortedDictionary<int, SortedDictionary<int, char>> tiles = new SortedDictionary<int, SortedDictionary<int, char>>();
+
+            public char this[int x, int y] {
+                get =>  tiles.TryGetValue(x, out var yzDict) &&
+                        yzDict.TryGetValue(y, out var value) ? value : 'w';
+
+                set {
+                    if (!tiles.TryGetValue(x, out var yzDict)) {
+                        tiles[x] = yzDict = new SortedDictionary<int, char>();
+                    }
+
+                    yzDict[y] = value;
+                }
+            }
+
+            public IEnumerable<int> GetXIndexes() {
+                return tiles.Keys;
+            }
+
+            public IEnumerable<int> GetYIndexes() {
+                return tiles.Values.SelectMany(y => y.Keys).Distinct();
+            }
+
+            public IEnumerator<char> GetEnumerator()
+            {
+                foreach(var y in tiles.Values) {
+                    foreach(var tile in y.Values) {
+                        yield return tile;
+
+                    }
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+
+        public Tiles InitialSetOfTiles = new Tiles();
+
         private string Init()
         {
             string data;
@@ -55,30 +97,56 @@ namespace AdventOfCode
                     }
                 }
 
-                if (!TileGrid.TryGetValue(x, out var yList)) {
-                    TileGrid[x] = yList = new SortedList<int, char>();
-                }
-                if (!yList.TryGetValue(y, out var tileFace)) {
-                    yList[y] = tileFace = 'w';
-                }
-
-                if (tileFace == 'w') {
-                    Console.WriteLine($"\n\n{line}\nTile: {x}, {y}: {tileFace} -> b");
-                    TileGrid[x][y] = 'b';
+                if (InitialSetOfTiles[x,y] == 'w') {
+                    Console.WriteLine($"\n\n{line}\nTile: {x}, {y}: {InitialSetOfTiles[x,y]} -> b");
+                    InitialSetOfTiles[x,y] = 'b';
                 } else {
-                    Console.WriteLine($"\n\n{line}\nTile: {x}, {y}: {tileFace} -> w");
-                    TileGrid[x][y] = 'w';
+                    Console.WriteLine($"\n\n{line}\nTile: {x}, {y}: {InitialSetOfTiles[x,y]} -> w");
+                    InitialSetOfTiles[x,y] = 'w';
                 }
             }
         }
 
         private void Task1()
         {
-            Console.WriteLine(TileGrid.SelectMany(x => x.Value).Count(x => x.Value == 'b'));
+            Console.WriteLine(InitialSetOfTiles.Count(x => x == 'b'));
         }
 
-        private void Task2()
+        private void Task2(int cycles)
         {
+             var oldTiles = this.InitialSetOfTiles;
+            foreach(var cycle in Enumerable.Range(0, cycles)) {
+                var newTiles = new Tiles();
+                var firstX = oldTiles.GetXIndexes().Min() - 1;
+                var lastX = oldTiles.GetXIndexes().Max() + 1;
+                var firstY = oldTiles.GetYIndexes().Min() - 1;
+                var lastY = oldTiles.GetYIndexes().Max() + 1;
+                for(var x = firstX; x <= lastX; x++) {
+                    for(var y = firstY; y <= lastY; y++ ) {
+                        var surroundingCubes = new List<char>();
+                        surroundingCubes.Add(oldTiles[x-1, y-1]);
+                        surroundingCubes.Add(oldTiles[x+1, y+1]);
+                        surroundingCubes.Add(oldTiles[x-1, y+1]);
+                        surroundingCubes.Add(oldTiles[x+1, y-1]);
+                        surroundingCubes.Add(oldTiles[x-2, y]);
+                        surroundingCubes.Add(oldTiles[x+2, y]);
+
+                        newTiles[x,y] = oldTiles[x,y];
+                        if (oldTiles[x, y] == 'w' && surroundingCubes.Where(tile => tile == 'b').Count() == 2) {
+                            newTiles[x, y] = 'b';
+                        }
+                        else if(oldTiles[x, y] == 'b') {
+                            if (surroundingCubes.Count(tile => tile == 'b') == 0 || surroundingCubes.Count(tile => tile == 'b') > 2 ){
+                                newTiles[x, y] = 'w';
+                            }
+                        }
+                    }
+                };
+
+                oldTiles = newTiles;
+            }
+
+            Console.WriteLine(oldTiles.Count(x => x == 'b'));
         }
 
         public void Run()
@@ -108,7 +176,7 @@ namespace AdventOfCode
             Console.WriteLine("\nLooking for answer for Task 1.");
             this.Task1();
             Console.WriteLine("\nLooking for answer for Task 2.");
-            this.Task2();
+            this.Task2(100);
         }
     }
 }
